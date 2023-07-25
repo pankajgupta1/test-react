2dedb76d85843ba36633fbf10feda357c60ef535
Pankaj Gupta <pankajgupta9898@gmail.com>
2023-07-25 16:27:30 +0530

    test cases updated of userlist

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,19 +1,35 @@
 import { render, screen, within } from "@testing-library/react"
 import UserList from "./UserList"
 
-test("render one user per row", async () => {
+const renderComponent = () => {
   const users = [
     { name: "jane", email: "jane@jane.com" },
     { name: "kite", email: "kite@jane.com" },
   ];
 
-  const { container } = render(<UserList users={users} />);
+  render(<UserList users={users} />);
+  return {users}
+}
+
+beforeEach(() => {
+  console.log('hii')
+})
+
+test("render one user per row", async () => {
+  renderComponent();
   // screen.logTestingPlaygroundURL()
 
-  const rows = container.querySelectorAll("tbody tr");
+  const rows = within(screen.getByTestId('users')).getAllByRole('row')
   expect(rows).toHaveLength(2);
 });
 
 test('each row renders user email and name', async () => {
-  render(<UserList users={[]} />)
+  const {users} = renderComponent()
+
+  users.forEach(user => {
+    const name = screen.getByRole('cell', {name: user.name})
+    const email = screen.getByRole('cell', {name: user.email})
+    expect(name).toBeInTheDocument()
+    expect(email).toBeInTheDocument()
+  })
 })
