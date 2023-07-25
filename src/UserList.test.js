161281783cef440c99a51cc27712f9c154c6f9cb
Pankaj Gupta <pankajgupta9898@gmail.com>
2023-07-25 16:00:47 +0530

    by query function get is added

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,19 +1,18 @@
 import { render, screen, within } from "@testing-library/react"
 import UserList from "./UserList"
 
-test('render one user per row', async () => {
+test("render one user per row", async () => {
   const users = [
-    {name: 'jane', email: 'jane@jane.com'},
-    {name: 'kite', email: 'kite@jane.com'},
-  ]
+    { name: "jane", email: "jane@jane.com" },
+    { name: "kite", email: "kite@jane.com" },
+  ];
 
-  render(<UserList users={users} />)
+  const { container } = render(<UserList users={users} />);
   // screen.logTestingPlaygroundURL()
 
-  const rows = within(screen.getByTestId('users')).getAllByRole('row')
-
-  expect(rows).toHaveLength(2)
-})
+  const rows = container.querySelectorAll("tbody tr");
+  expect(rows).toHaveLength(2);
+});
 
 test('each row renders user email and name', async () => {
   render(<UserList users={[]} />)
